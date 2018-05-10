8a6b3efb25ca4aa1ce2edbf6c382e25c8a08a8d3
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2018-05-10 15:04:21 +0100

    Add password recovery page

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,16 +1,27 @@
-$(document).ready(function() {
-    $('.user-button').on('click', function(e) {
-        title =  $('h3');
-        if (title.html() === 'Sign in') {
-            e.preventDefault()
-            title.html('Sign up');
-            $(this).html('<i class="fa fa-user"></i> Sign in');
-           $('#username').prop({'placeholder': 'Your username'});
-           $('label[for="username"]').html('Username');
-           $('.dark').html('Sign up');
-            $('.checkbox-recovery, .email').toggle();
-        } 
+$(document).ready(function () {
+  $('.user-button').on('click', function (e) {
+    title = $('h3');
+    id = e.target.id;
+    if (id === 'signup') {
+      e.preventDefault()
+      title.html('Sign up');
+      $(this).html('<i class="fa fa-sign-in"></i> Sign in');
+      $('#username').prop({ 'placeholder': 'Your username' });
+      $('label[for="username"]').html('Username');
+      $('.dark').html('Sign up');
+      $(this).prop({'id': 'signin'})
+      $('.checkbox-recovery, .email').toggle();
+    }
 
-        $('input:eq(0)').focus();
-    })
+    $('input:eq(0)').focus();
+  })
+
+  $('.recovery').on('click', function (e) {
+    e.preventDefault();
+    $('h3').html('Password recovery');
+    $('.dark').html('Continue');
+    $('.checkbox-recovery').toggle();
+    $('.form-group:eq(0), .form-group:eq(1), .form-group:eq(2)').toggle();
+    $('.user-button').replaceWith('<button class="user-button link" id="signin"><i class="fa fa-sign-in"></i> Sign in</button>')
+  })
 });
