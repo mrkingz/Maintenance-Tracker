aee45185db1152bf4632602051de651c1228a989
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2018-05-10 14:11:43 +0100

    Add sign up page

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+    $('.user-button').on('click', function(e) {
+        title =  $('h3');
+        if (title.html() === 'Sign in') {
+            e.preventDefault()
+            title.html('Sign up');
+            $(this).html('<i class="fa fa-user"></i> Sign in');
+           $('#username').prop({'placeholder': 'Your username'});
+           $('label[for="username"]').html('Username');
+           $('.dark').html('Sign up');
+            $('.checkbox-recovery, .email').toggle();
+        } 
 
+        $('input:eq(0)').focus();
+    })
 });
