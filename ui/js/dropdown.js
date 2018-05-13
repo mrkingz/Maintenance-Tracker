2e8193f32ea31121da6a3c5f8dd3c07a54d8e3f9
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2018-05-13 21:23:01 +0100

    add filter-controls

--- ui/js/dropdown.js
+++ ui/js/dropdown.js
@@ -1,39 +1,35 @@
 $(document).ready(function () {
-  $('#activities').hover(function (e) {
-    displayDropdown(e)
-  });
+  $('#activities-dropdown').hover(function (e) {
+    e.preventDefault();
+    displayDropdown(e, 'activities-menu')
+  })
+  // }, function(e) {
+  //   $("#"+$(e.target).siblings('.dropdown-content').prop('id')).hide();
+  // });
 
-
+  
   $('.dropdown-content a').on('click', function (e) {
     loadPage();
   })
 
-  window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
-      var dropdowns = $(".dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if ($(openDropdown).css('display') !== 'none') {
-          dropdowns.toggle();
-        }
-      }
-    }
-  } 
+  $('#filters-dropdown').hover(function(e) {
+    e.preventDefault()
+    displayDropdown(e, 'filters-menu')
+  })
 });
 
-function displayDropdown(e) {
+function displayDropdown(e, id) {
   const dropdown = $(e.target);
-  const content = $('.dropdown-content');
-
+  contentId = dropdown.siblings('.dropdown-content').prop('id')
+  const content = $('#'+id);
   let top = dropdown.position().top;
-  let height = parseInt(dropdown.css('height').replace('px', ''));
-  let width = parseInt(dropdown.css('width').replace('px', ''));
-  let contWidth = parseInt(content.css('width').replace('px', ''));
+  
+  let height = parseInt(dropdown.innerHeight());
+  let width = parseInt(dropdown.innerWidth());
+  let contWidth = parseInt(content.innerWidth());
   content.css({
     'top': (top + height) + 'px',
     'left': (dropdown.position().left - contWidth + width) + 'px',
   });
-  $('.dropdown-content').toggle();
+  content.toggle();
 }
