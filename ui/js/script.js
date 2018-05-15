7cef233eb9943b4fdfb220dc61ca0ceab0bdddc5
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2018-05-15 16:10:53 +0100

    Update script.js

--- ui/js/script.js
+++ ui/js/script.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
   $('.rTableCell button.link').on('click', function(e) {
     e.preventDefault()
     $('h3').html('Request Details') 
-    $('.details, .requests').toggle();
+    $('.request-details, .requests, .requests').toggle();
 
   })
 
